0065c2b4babef6599eb05b9717cca0251578cb65
Kurogami2134 <90238011+Kurogami2134@users.noreply.github.com>
2025-08-21 12:54:31 -0300

    Flipped write_once guard system. - write_once now checks if value is not equal to the first 2 bytes of data instead of checking if it's zeros.

--- ModIO/cwcheatio.py
+++ ModIO/cwcheatio.py
@@ -69,7 +69,7 @@ class CwCheatIO:
     def write_once(self, data: bytes) -> None:
         if self.virtual_address < 0x8800000:
             raise PermissionDenied
-        self.file.write(f'_L 0xE{hex(int(len(data)//4)).replace("0x", ""):0>3}0000 0x0{hex(self.virtual_address-0x8800000).replace("0x", ""):0>7}\n')
+        self.file.write(f'_L 0xE{hex(int(len(data)//4)).replace("0x", ""):0>3}{hex(data[1] << 8 | data[0]).replace("0x", ""):0>4} 0x1{hex(self.virtual_address-0x8800000).replace("0x", ""):0>7}\n')
         while len(data)//4 > 0:
             word = data[:4]
             data = data[4:]
